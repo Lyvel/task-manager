293c55563ed3c4db59f3160334e0e64e64c09dc7
Dawid Mleczko <dawid@lyvel.co.uk>
2023-11-28 22:22:07 +0000

    Mobile Design
    Updated CTA Section.

--- components/landing-page/landing-page-cta.tsx
+++ components/landing-page/landing-page-cta.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 
 export default function LandingPageCTA() {
   return (
-    <div className="bg-cta-pattern w-screen bg-cover bg-no-repeat text-white flex justify-center items-center flex-col py-20 gap-8">
-      <h1 className="text-center text-6xl font-extrabold">
+    <div className="bg-cta-pattern w-screen bg-cover bg-no-repeat text-white flex justify-center items-center flex-col py-14 lg:py-20 gap-8">
+      <h1 className="text-center text-[32px] lg:text-6xl font-extrabold">
         Ready to Transform Your Workflow?
       </h1>
-      <p className="leading-7 text-xl text-center w-[1100px]">
+      <p className="leading-7 text-xl text-center lg:w-[1100px]">
         Take the First Step with Task Forge and Experience Effortless Task
         Management. Join Now for a Smoother, More Productive Tomorrow.
       </p>
       <Link
         href={""}
-        className="bg-background text-foreground flex px-5 py-3 rounded-lg gap-2 hover:text-primary"
+        className="bg-background text-foreground flex px-5 py-3 mt-8 lg:mt-0 rounded-lg gap-2 hover:text-primary"
       >
         Get Started
         <ArrowRight />
