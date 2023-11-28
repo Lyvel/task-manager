4ab1c95ae6dd23ec2efb62e94cbda68a1cc3bb78
Dawid Mleczko <dawid@lyvel.co.uk>
2023-11-28 22:01:52 +0000

    Mobile Design
    Updated Why Choose Us Section.

--- components/landing-page/landing-page-whyus.tsx
+++ components/landing-page/landing-page-whyus.tsx
@@ -5,21 +5,23 @@ import si2 from "../../public/si2.png";
 
 export default function LandingPageWhyUs() {
   return (
-    <div className="flex w-[1100px] flex-col m-auto pt-24">
-      <h1 className="text-5xl font-bold text-center leading-[1.3]">
+    <div className="flex lg:w-[1100px] flex-col m-auto pt-24 px-4 lg:px-0">
+      <h1 className="text-[32px] lg:text-5xl font-bold text-center leading-[1.3]">
         Unveiling Task Forge, Your Ultimate Task Management Solution for Peak
         Productivity
       </h1>
-      <p className="pt-2 text-center text-xl font-light">
+      <p className="pt-6 lg:pt-2 text-center lg:text-xl font-light">
         Elevate Your Projects with OrganizeHub: The Ultimate Task Management
         Solution for Seamless Organization, Enhanced Collaboration, and
         Unmatched Productivity
       </p>
       <div className="flex flex-col gap-12 py-16">
-        <div className="bg-secondary rounded-lg p-10 flex justify-between">
-          <div className="flex flex-col justify-center gap-4 w-[492px]">
-            <h1 className="text-4xl font-semibold">Efficiency Amplified</h1>
-            <p className="leading-snug">
+        <div className="bg-secondary rounded-lg lg:p-10 p-4 flex flex-col lg:flex-row justify-between">
+          <div className="flex flex-col justify-center gap-6 lg:gap-4 lg:w-[492px]">
+            <h1 className="lg:text-4xl text-[28px] font-semibold">
+              Efficiency Amplified
+            </h1>
+            <p className="lg:leading-snug">
               Maximize your productivity with Task Forge&apos;s powerful
               features. From task prioritization to collaborative project
               management, every aspect is crafted to amplify efficiency and
@@ -28,9 +30,9 @@ export default function LandingPageWhyUs() {
           </div>
           <Image src={boc1} width={495} height={346} alt="si1" />
         </div>
-        <div className="bg-background rounded-lg p-10 flex justify-between">
+        <div className="bg-secondary rounded-lg lg:p-10 p-4 flex flex-col-reverse lg:flex-row justify-between">
           <Image src={si1} width={476} height={346} alt="Boc1" />
-          <div className="flex flex-col justify-center gap-4 w-[492px]">
+          <div className="flex flex-col justify-center gap-6 lg:gap-4 lg:w-[492px]">
             <h1 className="text-4xl font-semibold">Customizable Workflows</h1>
             <p className="leading-snug">
               Tailor Task Forge to fit your unique work style. Our customizable
@@ -40,8 +42,8 @@ export default function LandingPageWhyUs() {
             </p>
           </div>
         </div>
-        <div className="bg-secondary rounded-lg p-10 flex justify-between">
-          <div className="flex flex-col justify-center gap-4 w-[492px]">
+        <div className="bg-secondary rounded-lg lg:p-10 p-4 flex flex-col lg:flex-row justify-between">
+          <div className="flex flex-col justify-center gap-6 lg:gap-4 lg:w-[492px]">
             <h1 className="text-4xl font-semibold">Time Management Mastery</h1>
             <p className="leading-snug">
               Make control of your time like never before. Task Forge&apos;s
