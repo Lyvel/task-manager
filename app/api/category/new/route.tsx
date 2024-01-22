94bea803237332f62f33fc255508fa7d4307d062
Dawid Mleczko <dawid@lyvel.co.uk>
2024-01-22 16:28:31 +0000

    Fixed category colours not changing on creation.

--- app/api/category/new/route.tsx
+++ app/api/category/new/route.tsx
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, title, description } = body;
+    const { email, title, description, colour } = body;
     const nextTask = await db.categories.create({
-      data: { email, title, description },
+      data: { email, title, description, colour },
     });
     return NextResponse.json({
       message: "Category Added successfully",
